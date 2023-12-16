588b51ebbcc0dbec8cf7336ad265ad70e6c8d117
Simone Corbetta <simone.corbetta@gmail.com>
2023-12-16 16:21:43 +0100

    Test

--- dooku/scripts/renders.py
+++ dooku/scripts/renders.py
@@ -78,9 +78,6 @@ def define_env(env):
     print(f'path:    templates_dir={templates_dir}')
     print(f'path:    rtl_syn_dir={rtl_syn_dir}')
     print(f'path:    rtl_sim_dir={rtl_sim_dir}')
-    print(os.popen(f'ls -a {os.getcwd()}').read())
-    print(os.popen(f'ls -a {src_root}').read())
-    print(os.popen(f'ls -a {site_root}').read())
     print(os.popen(f'ls -a {rtl_syn_dir}').read())
     
     # Jinja engine
