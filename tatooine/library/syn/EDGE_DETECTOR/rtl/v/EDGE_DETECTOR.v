320f1d457b7baa4e4408f9aa80e987671c60a504
Simone Corbetta <simone.corbetta@gmail.com>
2024-02-19 09:46:34 +0100

    - Design fix. Edge detection requires one lesser clock cycle now

--- tatooine/library/syn/EDGE_DETECTOR/rtl/v/EDGE_DETECTOR.v
+++ tatooine/library/syn/EDGE_DETECTOR/rtl/v/EDGE_DETECTOR.v
@@ -2,33 +2,19 @@
 
 module EDGE_DETECTOR (
     input wire  CLK,
-    input wire  RSTN,
     input wire  SAMPLE_IN,
     output wire RISE_EDGE_OUT,
     output wire FALL_EDGE_OUT
 );
 
-    wire        rise_edge_out;
-    wire        fall_edge_out;
-    reg [1:0]   sample_follower;
+    reg sample_in;
     
     always @(posedge CLK) begin
-        if(!RSTN) begin
-            sample_follower <= 2'b00;
-        end
-        else begin
-            // The new sample goes into [0], the old one goes into [1]
-            sample_follower <= { sample_follower[0], SAMPLE_IN };
-        end
+        sample_in <= SAMPLE_IN;
     end
     
-    // Detect edges without introducing further delay
-    assign rise_edge_out = (sample_follower == 2'b01);
-    assign fall_edge_out = (sample_follower == 2'b10);
-    
-    // Pinout
-    assign RISE_EDGE_OUT = rise_edge_out;
-    assign FALL_EDGE_OUT = fall_edge_out;
+    assign RISE_EDGE_OUT = ~sample_in & SAMPLE_IN;
+    assign FALL_EDGE_OUT = sample_in & ~SAMPLE_IN;
 endmodule
 
 `default_nettype wire
